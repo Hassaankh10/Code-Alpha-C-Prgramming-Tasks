da1530ff9b11d16021b5f8545efced045a41fd30
hassaankh10 <m.hassaan10.mh10@gmail.com>
2024-06-16 00:05:39 +0500

    for loops for entering subject grades made

--- CGPA-calculator.cpp
+++ CGPA-calculator.cpp
@@ -1,10 +1,28 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"/t/twelcome to CGPA Calculator"<<endl;
-cout<<"Enter Your Name"<<endl;
+string name;
+int scount;
+cout<<"\twelcome to CGPA Calculator"<<endl;
+cout<<"Enter Your Name:";
+cin>>name;
+cout<<"Enter the semester you are in:";
+cin>>scount;
+for (int  i = 0; i < scount; i++)
+{   
+    cout<<"\nEnter the number of courses you took in semester "<<i+1<<":";
+    int numberofsubjects;
+    cin>>numberofsubjects;
+    for (int  i = 0; i < numberofsubjects; i++)
+    {
+    char grade[numberofsubjects];
+    int credits[numberofsubjects];
+    cout<<"\nEnter the grade in Course "<<i+1<<" with respect to creits earned:";
+    cin>>grade[i]>>credits[i];   
+     
+    }
+    
 
-
-
+}
 return 0;
 }
