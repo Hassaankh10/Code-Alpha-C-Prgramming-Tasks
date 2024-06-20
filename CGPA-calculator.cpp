9e8db0d5299c993ce5505c1780e2f0d4676b9005
hassaankh10 <m.hassaan10.mh10@gmail.com>
2024-06-20 13:39:22 +0500

    CGPA-calculator completed

--- CGPA-calculator.cpp
+++ CGPA-calculator.cpp
@@ -1,28 +1,46 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-string name;
-int scount;
-cout<<"\twelcome to CGPA Calculator"<<endl;
-cout<<"Enter Your Name:";
-cin>>name;
-cout<<"Enter the semester you are in:";
-cin>>scount;
-for (int  i = 0; i < scount; i++)
-{   
-    cout<<"\nEnter the number of courses you took in semester "<<i+1<<":";
-    int numberofsubjects;
-    cin>>numberofsubjects;
-    for (int  i = 0; i < numberofsubjects; i++)
-    {
-    char grade[numberofsubjects];
-    int credits[numberofsubjects];
-    cout<<"\nEnter the grade in Course "<<i+1<<" with respect to creits earned:";
-    cin>>grade[i]>>credits[i];   
-     
-    }
-    
 
-}
-return 0;
+int main() {
+    string name;
+    int scount;
+    cout << "\tWelcome to CGPA Calculator" << endl;
+    cout << "Enter Your Name: ";
+    cin >> name;
+    cout << "Enter the semester you are in: ";
+    cin >> scount;
+
+    float GPA[scount];
+    float CGPA = 0;
+    int totalCredits = 0;
+
+    for (int i = 0; i < scount; i++) {
+        cout << "\nEnter the number of courses you took in semester " << i + 1 << ": ";
+        int numberOfSubjects;
+        cin >> numberOfSubjects;
+
+        int credits[numberOfSubjects];
+        double grades[numberOfSubjects];
+
+        float semesterCredits = 0;
+        float semesterGPA = 0;
+
+        for (int j = 0; j < numberOfSubjects; j++) {
+            cout << "\nEnter the grade and credits for Course " << j + 1 << ": ";
+            cin >> grades[j] >> credits[j];
+            semesterCredits += credits[j];
+            semesterGPA += grades[j] * credits[j];
+        }
+
+        GPA[i] = semesterGPA / semesterCredits;
+        totalCredits += semesterCredits;
+        CGPA += semesterGPA;
+
+        cout << "\nThe GPA for semester " << i + 1 << " is: " << GPA[i] << endl;
+    }
+
+    CGPA /= totalCredits;
+    cout << "\nThe CGPA is: " << CGPA << endl;
+
+    return 0;
 }
